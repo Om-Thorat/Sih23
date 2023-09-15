596b06b0b3dd0559b7a70c5971927eb542b437f9
Aryan Pandit <a12022005@gmail.com>
2023-09-15 21:54:41 +0530

    farmer page

---
+++ client/src/lib/Farmer.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import Grid from "./Grid.svelte";
+
+
+</script>
+
+
+<div class="flex flex-row min-h-[70%] w-screen items-center justify-evenly">
+    <Grid />
+</div>
---
+++ client/src/lib/Grid.svelte
@@ -0,0 +1,34 @@
+<div class="z-101 leftbox">
+    <div class="  h-75">
+    <input type="file" name="image">
+    </div>
+</div>
+<div class=" h-105 w-50">
+<ul class="mt-8 grid md:gridcols-2 gap-3 grid-rows-3 md:gap-5 ">
+    
+                
+        <li class=" grid col-1 bg-black rounded-lg shadow-xl">
+             <div class="h-24">
+                <input class=" bg-black rounded-lg shadow-xl text- center h-35 w-45" type="text" placeholder="Reviewed crops" >
+            </div>
+        
+        </li>
+  
+            
+        <li class="bg-black rounded-lg shadow-xl">
+            <div>
+                <input class=" bg-black rounded-lg shadow-xl text- center h-35 w-45" type="text" placeholder="Yet to be reviewed" >
+        
+          </div>
+        </li>
+    
+        <li class="bg-black rounded-lg shadow-xl">
+            <div>
+                <input class=" bg-black rounded-lg shadow-xl text- center h-35 w-45" type="text" placeholder="Something's here" >
+            
+            </div>
+        </li>
+    
+       
+</ul>
+</div>
