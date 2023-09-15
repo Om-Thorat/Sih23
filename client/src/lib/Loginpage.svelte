66f6d44d97f187cbbbc513f1fcae74ec9b3a416e
Aryan Pandit <a12022005@gmail.com>
2023-09-16 04:28:19 +0530

    added title

--- client/src/lib/Loginpage.svelte
+++ client/src/lib/Loginpage.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+import Title from "./Title.svelte";
 import {state} from "../store";
 
 function Goin(): null {
@@ -6,7 +7,7 @@ function Goin(): null {
   return null;
 }
 </script>
-
+<Title />
 <div class="flex flex-row min-h-[70%] w-screen items-center justify-evenly">
     <button class="w-1/5 aspect-square rounded-full border-white border-2 transition-all hover:border-opacity-0" on:click={Goin}>
         <img src="/Farmer.png" alt="Farmer" class="scale-90 transition-all cursor-pointer hover:scale-100" />
