207cd8a12c35157122e7a07c2b7249f99ae129fc
Om <thoratom1104@gmail.com>
2023-09-16 14:24:58 +0530

    switch to bcrypt

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, send_from_directory, request
 import pymongo
-import argon2
+import bcrypt as bc
 import os
 from datetime import datetime
 app = Flask(__name__)
 
-ph = argon2.PasswordHasher()
 
 client = pymongo.MongoClient((os.environ['MONGODB_URI']))
 db = client['Track01']
@@ -30,7 +29,10 @@ def login():
     user = request.args.get("user");
     pas = request.args.get("pass");
     job = request.args.get("job");
-    hpas = ph.hash(pas);
+    bytes = pas.encode('utf-8');
+    salt = bc.gensalt();
+    hpas = bc.hashpw(bytes, salt);
+    print(type(hpas))
     users = db.users
     entryform = {
         "user": user,
@@ -40,17 +42,14 @@ def login():
     entry = users.find_one({"user":user})
     if not entry:
         users.insert_one(entryform)
+        return ["hehe"]
     else:
         if entry['job'] != job:
             return ["nope","Wrong job"]
-        try:
-            ph.verify(entry['pass'],pas)
-            if ph.check_needs_rehash(entry['pass']):
-                entry['pass'] = ph.hash(pas);
-                users.update_one({"user":user},{"$set":{ "pass": entry['pass'] }},upsert=False)
+        if (bc.checkpw(pas.encode('utf-8'),entry['pass'])):
             return ["yep",str(entry['job'])]
-        except argon2.exceptions.VerifyMismatchError:
+        else:
             return ["nope","You mistyped it maybe? Try again"]
         
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
